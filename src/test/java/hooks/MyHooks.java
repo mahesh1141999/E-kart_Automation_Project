05292f01f1661b3d8085d637b49b4e7cc1df9249
Mahesh <mahesh1141999@gmail.com>
2025-11-19 19:46:31 +0530

    Wait time set to 5 sec

--- src/test/java/hooks/MyHooks.java
+++ src/test/java/hooks/MyHooks.java
@@ -21,7 +21,7 @@ public class MyHooks {
         driver = DriverFactory.getDriver();
         driver.manage().deleteAllCookies();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get(prop.getProperty("url"));
     }
 
